02a37c1b8ea14b8060aa77f1471e860c247331ce
Emily Reyes <emilymwind1@gmail.com>
2023-04-12 17:09:25 -0600

    Add project files.

---
+++ BurialData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onnx_supervised
+{
+    public class BurialData
+    {
+        public float depth { get; set; }
+        public float length { get; set; }
+        public float area_NE { get; set; }
+        public float area_NW { get; set; }
+        public float area_SE { get; set; }
+        public float area_SW { get; set; }
+        public float wrapping_B { get; set; }
+        public float wrapping_H { get; set; }
+        public float wrapping_W { get; set; }
+        public float ageatdeath_A { get; set; }
+        public float ageatdeath_C { get; set; }
+        public float ageatdeath_I { get; set; }
+        public float ageatdeath_IN { get; set; }
+        public float ageatdeath_N { get; set; }
+
+        public Tensor<float> AsTensor()
+        {
+            float[] data = new float[]
+            {
+                depth, length, area_NE, area_NW, area_SE, area_SW, wrapping_B, wrapping_H, wrapping_W,
+                ageatdeath_A, ageatdeath_C, ageatdeath_I, ageatdeath_IN, ageatdeath_N
+            };
+            int[] dimensions = new int[] { 1, 14 };
+            return new DenseTensor<float>(data, dimensions);
+        }
+    }
+}
---
+++ Prediction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onnx_supervised
+{
+
+    public class Prediction
+    {
+        public string PredictedValue { get; set; }
+    }
+
+}
